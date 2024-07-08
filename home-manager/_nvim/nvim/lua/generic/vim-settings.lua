cf406a9f97d350bfd1441cc4acf75544f436eccb
ncarchar <carsoncmiller@proton.me>
2024-07-08 17:44:30 -0400

    146 current  2024-07-07 21:41:34  24.05.2150.89c49874fb15  6.6.35                          *

--- home-manager/_nvim/nvim/lua/generic/vim-settings.lua
+++ home-manager/_nvim/nvim/lua/generic/vim-settings.lua
@@ -13,7 +13,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 vim.opt.list = true
 vim.opt.listchars:append("eol:↲")
