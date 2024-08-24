4a6651ea9075547c96c37e68a59b57059da0d821
ncarchar <carsoncmiller@proton.me>
2024-08-23 20:56:59 -0400

    123acba275c2310e484c8d466d9b3dc1  -6 current   2024-08-23 16:58:38  24.05.4169.797f7dc49e0b  6.6.46                          *

--- home-manager/nix/home.nix
+++ home-manager/nix/home.nix
@@ -5,6 +5,8 @@
   home.stateVersion = "24.05";
   home.file = {
     ".config/nvim".source = ./../_nvim/nvim;
+    ".config/waybar".source = ./../_waybar/waybar;
+    ".config/hypr".source = ./../_hypr/hypr;
     ".prettierrc".source = ./../_prettier/.prettierrc;
     ".scripts".source = ./../_scripts/.scripts;
     ".tmux.conf".source = ./../_tmux/.tmux.conf;
