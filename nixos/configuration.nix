ee0ab53cacbbbf7b1dabd6383cff7c7db31424a7
ncarchar <carsoncmiller@proton.me>
2024-08-24 17:28:11 -0400

    9cf2abff8015c8f7d8254d20ad09a17d  -15 current  2024-08-24 14:50:10  24.05.4169.797f7dc49e0b  6.6.46                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
       /etc/nixos/hardware-configuration.nix
       /etc/nixos/system-packages.nix
       /etc/nixos/vm.nix
+      /etc/nixos/zsh.nix
     ];
   nixpkgs.config = {
     packageOverrides = pkgs: {
@@ -161,35 +162,5 @@
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
-  };
-
-  location = {
-    provider = "manual";
-    latitude = 40.4406;
-    longitude = -79.9959;
-  };
-
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    promptInit = "export PATH=$HOME/.scripts:$PATH\neval \"$(zoxide init zsh)\"\nsource $HOME/.scripts/fzf-git.sh";
-    ohMyZsh = {
-      theme = "robbyrussell";
-      enable = true;
-      plugins = [ ];
-    };
-    shellAliases = {
-      ls = "exa --across --group-directories-first";
-      la = "exa --all --across  --group-directories-first";
-      ll = "exa -l --all -g --icons --git --no-user --classify --group-directories-first";
-      lt = "exa --level=1 --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
-      llt = "exa --level=2 --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
-      lft = "exa --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
-      hist = ". hist";
-      tms = ". tms";
-    };
   };
 }
