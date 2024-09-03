0ce8bf77180463c79a3292208326069400a37897
ncarchar <carsoncmiller@proton.me>
2024-09-02 21:18:30 -0400

    6076d716397f388acd1a7e41a62c242e  -37 current  2024-09-02 21:18:25  24.05.4169.797f7dc49e0b  6.6.46                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 {
   imports =
@@ -18,6 +14,7 @@
       };
     };
   };
+  nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   programs.nix-ld.enable = true;
 
@@ -26,31 +23,12 @@
   boot.loader.grub.useOSProber = true;
   networking.hostName = "nixos";
 
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -60,18 +38,13 @@
     pulse.enable = true;
   };
 
-  # docker
   virtualisation.docker.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ncarchar = {
     isNormalUser = true;
     description = "Carson Miller";
     extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" ];
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
@@ -85,43 +58,25 @@
     };
   };
 
-  # This value determines the NixOS release from which the default
   system.stateVersion = "24.05";
 
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.gdm.enableGnomeKeyring = true; # load gnome-keyrin
+  security.pam.services.gdm.enableGnomeKeyring = true;
   security.pam.services.gdm-password.enableGnomeKeyring = true;
-
-  # programs.hyprland = {
-  #   enable = true;
-  #   xwayland.enable = true;
-  # };
 
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
   };
-
-  services = {
-    displayManager = {
-      sddm.enable = true;
-      sddm.wayland.enable = true;
-      sddm.theme = "where_is_my_sddm_theme";
-    };
-  };
-
-  hardware.opengl.enable = true;
-
-  systemd.extraConfig = ''
-    DefaultTimeoutStopSec=10s
-  '';
-
   programs.seahorse.enable = true;
   programs.dconf.enable = true;
   programs.neovim.enable = true;
   environment.variables.EDITOR = "nvim";
   programs.firefox.enable = true;
   programs.thunar.enable = true;
+  programs.steam = {
+    enable = true;
+  };
 
   programs.git = {
     enable = true;
@@ -139,20 +94,13 @@
     };
   };
 
-  programs.steam = {
-    enable = true;
-  };
-
-  programs.gamemode.enable = true;
-
-  # automatic updates and removal of old builds
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
 
-  # config for printer
+  services.printing.enable = true;
   services.avahi = {
     enable = true;
     nssmdns4 = true;
