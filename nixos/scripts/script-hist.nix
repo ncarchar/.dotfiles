dc932d6627dd34d975eed0b7bd4ceab39f0daea3
ncarchar <carsoncmiller@proton.me>
2024-09-04 18:09:41 -0400

    c12b561ede3f663424f656614d573696  -42 current  2024-09-04 18:09:37  24.05.4169.797f7dc49e0b  6.6.46                          *

--- nixos/scripts/script-hist.nix
+++ nixos/scripts/script-hist.nix
@@ -1,13 +1,16 @@
 { pkgs }:
-
 pkgs.writeShellApplication {
   name = "hist-nix";
   runtimeInputs = [ pkgs.zsh pkgs.fzf pkgs.gnused pkgs.gawk pkgs.wl-clipboard ];
   text = ''
     #!/usr/bin/env zsh
+    HISTFILE="$HOME/.zsh_history"
+    if [[ ! -f "$HISTFILE" ]]; then
+      echo "History file not found: $HISTFILE"
+      exit 1
+    fi
 
-    selected=$(fc -ln -t '%F %T' | fzf --border --ansi --height 40% | sed 's/[^ ]* [^ ]* //' | awk '{$1=$1};1')
-
+    selected=$(tac "$HISTFILE" | sed 's/^: [0-9]*:[0-9];//' | fzf --border --ansi --height 40% | sed 's/[^ ]* [^ ]* //' | awk '{$1=$1};1')
     if [[ -n $selected ]]; then
         echo -n "$selected" | wl-copy
         echo "'$selected' copied to clipboard."
