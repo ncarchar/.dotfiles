a77142c0047ae1b814fb2e62ce70cfddf7cc9e6a
ncarchar <carsoncmiller@proton.me>
2024-09-04 18:16:02 -0400

    e7c066b2c62276c35846b8f228b90357  -47 current  2024-09-04 18:15:59  24.05.4169.797f7dc49e0b  6.6.46                          *

--- nixos/zsh.nix
+++ nixos/zsh.nix
@@ -19,6 +19,7 @@
       lt = "exa --level=1 --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
       llt = "exa --level=2 --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
       lft = "exa --icons --tree --git --git-ignore --no-user --no-permissions --group-directories-first -a";
+      hist = ". hist";
       tms = ". tms";
     };
   };
