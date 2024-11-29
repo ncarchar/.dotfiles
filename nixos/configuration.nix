74a19ebd8b7453bb0c8c527f010a8ff4bb886014
Carson Miller <carsoncmiller@proton.me>
2024-11-29 18:03:49 -0500

    b3e6b8f9d07ece31a9294ccb8c5fbb1f  -43 current  2024-11-29 18:03:46  24.05.5709.c0b1da36f7c3  6.6.56                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,7 +121,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 7d";
   };
 
   services.printing.enable = true;
