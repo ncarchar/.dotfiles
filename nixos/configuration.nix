09df5deabf4bef16f70b195c54ded99e451e546e
Carson Miller <carsoncmiller@proton.me>
2025-01-20 18:01:26 -0500

    dd8be1bfb8df6843920d6cc7a0f6bffc  -174 current  2025-01-20 18:01:25  24.11.713184.e24b4c09e963  6.6.71                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -37,118 +37,122 @@
   };
 
   virtualisation.docker.enable = true;
-  hardware.sane = {
+  services.saned = {
     enable = true;
-    snapshot = true;
-    brscan5.enable = true;
-    brscan4.enable = true;
-    dsseries.enable = true;
-  };
-
-  users.users.ncarchar = {
-    isNormalUser = true;
-    home = "/home/ncarchar";
-    description = "Carson Miller";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" ];
-  };
-
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
-
-  fonts.fontconfig = {
-    defaultFonts = {
-      serif = [ "Liberation Serif" "Vazirmatn" ];
-      sansSerif = [ "Ubuntu" "Vazirmatn" ];
-      monospace = [ "JetBrainsMono" ];
-    };
-  };
-
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.gdm.enableGnomeKeyring = true;
-  security.pam.services.gdm-password.enableGnomeKeyring = true;
-
-  # i3 + picom
-  services.displayManager.defaultSession = "none+i3";
-  services.picom = {
-    enable = true;
-    settings = {
-      backend = "glx";
-      vsync = true;
-    };
-  };
-  services.xserver = {
-    enable = true;
-    desktopManager = {
-      xterm.enable = false;
-    };
-    windowManager.i3 = {
+    extraConfig = ''
+      usb 0x04f9 0x0468
+    '';
+      };
+    hardware.sane = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3blocks
-      ];
+      brscan5.enable = true;
+      dsseries.enable = true;
     };
-  };
 
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
-      theme = "robbyrussell";
+    users.users.ncarchar = {
+      isNormalUser = true;
+      home = "/home/ncarchar";
+      description = "Carson Miller";
+      extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" ];
+    };
+
+    fonts.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+
+    fonts.fontconfig = {
+      defaultFonts = {
+        serif = [ "Liberation Serif" "Vazirmatn" ];
+        sansSerif = [ "Ubuntu" "Vazirmatn" ];
+        monospace = [ "JetBrainsMono" ];
+      };
+    };
+
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.gdm.enableGnomeKeyring = true;
+    security.pam.services.gdm-password.enableGnomeKeyring = true;
+
+    # i3 + picom
+    services.displayManager.defaultSession = "none+i3";
+    services.picom = {
       enable = true;
-      plugins = [ ];
-    };
-  };
-
-  programs.neovim.enable = true;
-  environment.variables.EDITOR = "nvim";
-  environment.variables.GTK_THEME = "Adwaita:dark";
-  programs.firefox.enable = true;
-  programs.thunar.enable = true;
-  programs.steam.enable = true;
-  location = {
-    provider = "manual";
-    longitude = -80.0;
-    latitude = 40.0;
-  };
-  services.redshift = {
-    enable = true;
-    temperature.night = 5000;
-    temperature.day = 6500;
-    brightness.night = "0.9";
-    brightness.day = "1";
-  };
-
-  programs.git = {
-    enable = true;
-    config = {
-      init = {
-        defaultBranch = "main";
-      };
-      core = {
-        pager = "less -F -X";
-      };
-      "includeIf \"gitdir:/home/ncarchar/gatech/**\"" = {
-        path = "/home/ncarchar/gatech/.gitconfig";
+      settings = {
+        backend = "glx";
+        vsync = true;
       };
     };
-  };
+    services.xserver = {
+      enable = true;
+      desktopManager = {
+        xterm.enable = false;
+      };
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3blocks
+        ];
+      };
+    };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
+    users.defaultUserShell = pkgs.zsh;
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+        theme = "robbyrussell";
+        enable = true;
+        plugins = [ ];
+      };
+    };
 
-  services.printing.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-}
+    programs.neovim.enable = true;
+    environment.variables.EDITOR = "nvim";
+    environment.variables.GTK_THEME = "Adwaita:dark";
+    programs.firefox.enable = true;
+    programs.thunar.enable = true;
+    programs.steam.enable = true;
+    location = {
+      provider = "manual";
+      longitude = -80.0;
+      latitude = 40.0;
+    };
+    services.redshift = {
+      enable = true;
+      temperature.night = 5000;
+      temperature.day = 6500;
+      brightness.night = "0.9";
+      brightness.day = "1";
+    };
+
+    programs.git = {
+      enable = true;
+      config = {
+        init = {
+          defaultBranch = "main";
+        };
+        core = {
+          pager = "less -F -X";
+        };
+        "includeIf \"gitdir:/home/ncarchar/gatech/**\"" = {
+          path = "/home/ncarchar/gatech/.gitconfig";
+        };
+      };
+    };
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    services.printing.enable = true;
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+  }
