f4b48f5c105f321b48d73d965e9d010a8250a4f4
Carson Miller <carsoncmiller@proton.me>
2025-01-25 17:39:41 -0500

    372dc2c33e1aceed055a0b76d6af24d2  -182 current  2025-01-25 17:38:53  24.11.713184.e24b4c09e963  6.6.71                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,6 +113,7 @@
   programs.neovim.enable = true;
   environment.variables.EDITOR = "nvim";
   environment.variables.GTK_THEME = "Adwaita:dark";
+  environment.variables.NIX_SHELL_PRESERVE_PROMPT = "1";
   environment.sessionVariables.NIX_SHELL_PRESERVE_PROMPT = "1";
   programs.firefox.enable = true;
   programs.thunar.enable = true;
