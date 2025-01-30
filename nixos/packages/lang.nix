3758f81b9c8e20c7cccef822bdb361c85a6e08ad
Carson Miller <carsoncmiller@proton.me>
2025-01-29 19:46:32 -0500

    2660a102697cce09578555771723d506  -215 current  2025-01-29 19:46:30  24.11.713549.035f8c0853c2  6.6.72                          *

--- nixos/packages/lang.nix
+++ nixos/packages/lang.nix
@@ -2,19 +2,17 @@
 {
   environment.systemPackages = with pkgs; [
     cargo
-    go
     jdk21
     maven
     nodePackages."@angular/cli"
     nodePackages."eslint"
-    nodePackages."http-server"
     nodePackages."prettier"
     nodePackages."typescript"
     nodejs
     rustc
     zig
-    # (python3.withPackages (ps: with ps; [
-    #   pip
-    # ]))
+    (python3.withPackages (ps: with ps; [
+      pip
+    ]))
   ];
 }
