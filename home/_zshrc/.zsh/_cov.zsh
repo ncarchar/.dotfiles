90f33bd120f09b5c82d38063e9a206b2c0cf8c6d
Carson Miller <carson.miller@covestro.com>
2025-02-03 08:18:40 -0500

    b537d6c726b4a6503c84a31bebe41674 __

--- home/_zshrc/.zsh/_cov.zsh
+++ home/_zshrc/.zsh/_cov.zsh
@@ -1,3 +1,9 @@
+export COV=true
+
+export CHROME_BIN=/usr/bin/chromium-browser
+export BROWSER="/mnt/c/Users/cvhew/AppData/Local/Mozilla Firefox/firefox.exe"
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 alias sjava21="sdk use java 21.0.2-amzn"
 alias sjava17="sdk use java 17.0.10-amzn"
@@ -5,10 +11,5 @@ alias sjava8="sdk use java 8.0.412-amzn"
 
 export PATH=$JAVA_HOME/bin:$PATH
 
-export CHROME_BIN=/usr/bin/chromium-browser
-
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-export BROWSER="/mnt/c/Users/cvhew/AppData/Local/Mozilla Firefox/firefox.exe"
-export PATH="$PATH:/opt/nvim-linux64/bin"
--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,7 @@ set -e
 git --no-pager diff -U0
 
 if [[ -f "/etc/NIXOS" ]]; then
-    echo "tebuilding NixOS configuration..."
+    echo "rebuilding nixos configuration..."
     sudo nixos-rebuild switch
     gen=$(nixos-rebuild list-generations | grep current)
 else
