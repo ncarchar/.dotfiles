7a073828056e11f6126fa5b25668bb83338436f2
Carson Miller <carsoncmiller@proton.me>
2025-02-04 10:38:33 -0500

    2bad0a2572c72f869cbd6e5d8aed8af3 __

--- home-manager/certs.nix
+++ home-manager/certs.nix
@@ -1,4 +1,5 @@
 { config, pkgs, lib, ... }:
+
 let
   certsDir = "${config.xdg.configHome}/.certs";
   javaTrustStore = "${certsDir}/cacerts";
@@ -7,7 +8,8 @@ let
     "http://pki-services.covestro.net/grouppki/covestro_serverca.cer"
     "http://pki-services.covestro.net/grouppki/covestro_rootca.cer"
   ];
-in {
+in
+{
   home.packages = with pkgs; [
     jdk21
     curl
@@ -19,20 +21,27 @@ in {
 
     mkdir -p ${certsDir}
 
-    # Use absolute path for curl from nixpkgs to ensure it's available
+    # Use absolute paths for curl and keytool from nixpkgs
     CURL=${pkgs.curl}/bin/curl
     KEYTOOL=${pkgs.jdk21}/bin/keytool
 
     for url in ${lib.concatStringsSep " " (map (u: "\"${u}\"") certUrls)}; do
       filename=$(basename "$url")
+      alias="cert-$(basename "$filename" .crt)"
       dest="${certsDir}/$filename"
       
       echo "Downloading certificate from $url..."
       $CURL -k -o "$dest" "$url"
 
       if [ -f "$dest" ]; then
-        echo "Importing $filename into Java keystore..."
-        $KEYTOOL -importcert -trustcacerts -keystore "${javaTrustStore}" -storepass changeit -noprompt -alias "cert-$(basename "$filename" .crt)" -file "$dest"
+        echo "Checking if alias $alias already exists in keystore..."
+        
+        if $KEYTOOL -list -keystore "${javaTrustStore}" -storepass changeit -alias "$alias" >/dev/null 2>&1; then
+          echo "Certificate $alias already exists in keystore. Skipping import."
+        else
+          echo "Importing $filename into Java keystore..."
+          $KEYTOOL -importcert -trustcacerts -keystore "${javaTrustStore}" -storepass changeit -noprompt -alias "$alias" -file "$dest"
+        fi
       else
         echo "Failed to download certificate from $url"
       fi
@@ -44,8 +53,4 @@ in {
   home.sessionVariables = {
     JAVA_TOOL_OPTIONS = "-Djavax.net.ssl.trustStore=${javaTrustStore} -Djavax.net.ssl.trustStorePassword=changeit";
   };
-
-  # shellInit = ''
-  #   export JAVA_TOOL_OPTIONS="-Djavax.net.ssl.trustStore=${javaTrustStore} -Djavax.net.ssl.trustStorePassword=changeit"
-  # '';
 }
