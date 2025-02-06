43c2aef9f4f6fbc29ea9c38f84012183cbcf8d0a
Carson Miller <carsoncmiller@proton.me>
2025-02-06 18:07:55 -0500

    5cbd5046f7285b0b8796be9f33f44486 239 current  2025-02-05 18:21:55  24.11.713549.035f8c0853c2  6.6.72                          *

--- nixos/nix/vm.nix
+++ nixos/nix/vm.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.dconf.enable = true;
 
@@ -10,7 +10,7 @@
     spice-protocol
     win-virtio
     win-spice
-    gnome.adwaita-icon-theme
+    adwaita-icon-theme
   ];
 
   virtualisation = {
