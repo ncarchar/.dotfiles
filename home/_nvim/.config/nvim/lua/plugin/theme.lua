b9b247c44498c48e9ec314f9d74457bb84d7bdc4
Carson Miller <carsoncmiller@proton.me>
2025-05-28 20:29:25 -0400

    9d18f70515a657fe98f89899bdb28705 326 current  2025-05-28 19:35:05  24.11.718195.f09dede81861  6.6.92                          *

--- home/_nvim/.config/nvim/lua/plugin/theme.lua
+++ home/_nvim/.config/nvim/lua/plugin/theme.lua
@@ -3,7 +3,10 @@ return {
         'navarasu/onedark.nvim',
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme 'onedark'
+            require('onedark').setup {
+                style = 'dark'
+            }
+            require('onedark').load()
         end
     },
 }
