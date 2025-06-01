a3d48c7a57e37e87cb57eb2269def323624470e0
Carson Miller <carsoncmiller@proton.me>
2025-05-31 20:06:41 -0400

    b490cdd150c06b1cb1f31f5c1e2104b0 337 current  2025-05-31 19:23:05  25.11pre808367.59138c7667b7  6.12.30                          *

--- home/_nvim/.config/nvim/lua/plugin/lsp.lua
+++ home/_nvim/.config/nvim/lua/plugin/lsp.lua
@@ -4,7 +4,7 @@ return {
         opts = {},
         branch = "v1.x",
         dependencies = {
-            "mason-org/mason.nvim",
+            { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
             'folke/neodev.nvim',
         },
