0b11fa5f5d710874153c60b973da8d5248b316f4
Carson Miller <carsoncmiller@proton.me>
2025-06-03 15:37:59 -0400

    ffff2ec63f55caca06904443fc73db52 __

--- home/_nvim/.config/nvim/lua/plugin/lsp.lua
+++ home/_nvim/.config/nvim/lua/plugin/lsp.lua
@@ -120,26 +120,10 @@ return {
 				severity_sort = true,
 				float = { source = "if_many" },
 				underline = false,
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "󰅚 ",
-						[vim.diagnostic.severity.WARN] = "󰀪 ",
-						[vim.diagnostic.severity.INFO] = "󰋽 ",
-						[vim.diagnostic.severity.HINT] = "󰌶 ",
-					},
-				},
+				signs = true,
 				virtual_text = {
 					source = "if_many",
-					spacing = 1,
-					format = function(diagnostic)
-						local diagnostic_message = {
-							[vim.diagnostic.severity.ERROR] = diagnostic.message,
-							[vim.diagnostic.severity.WARN] = diagnostic.message,
-							[vim.diagnostic.severity.INFO] = diagnostic.message,
-							[vim.diagnostic.severity.HINT] = diagnostic.message,
-						}
-						return diagnostic_message[diagnostic.severity]
-					end,
+					spacing = 2,
 				},
 			})
 
