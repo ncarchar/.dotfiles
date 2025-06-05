ab58213131a1f0d5a4a61eabd77efbb3c105e101
Carson Miller <carsoncmiller@proton.me>
2025-06-05 15:54:52 -0400

    20989570ae3e4a67b9dee6d920328ed9 __

--- home/_nvim/.config/nvim/queries/java/injections.scm
+++ home/_nvim/.config/nvim/queries/java/injections.scm
@@ -1,14 +1,9 @@
 ; ~/.config/nvim/queries/java/injections.scm
 
 ((multiline_string_fragment) @injection.content
- (#match? @injection.content "<[^>]*>")
+ (#match? @injection.content "<")
  (#set! injection.language "html"))
 
-; ((string_literal) @injection.content
-;  (#match? @injection.content "<[^>]*>")
-;  (#set! injection.language "html"))
-;
-; ; For multiline content, use this pattern instead:
-; ((string_literal) @injection.content
-;  (#match? @injection.content "<.*>")
-;  (#set! injection.language "html"))
+((multiline_string_fragment) @injection.content
+ (#match? @injection.content "</.*>")
+ (#set! injection.language "html"))
