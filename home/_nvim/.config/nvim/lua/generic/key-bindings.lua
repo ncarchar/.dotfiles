f37f38a967f4d6b12032f87a2515de24232d0356
Carson Miller <carsoncmiller@proton.me>
2025-08-17 08:25:06 -0400

    0dff8908f67b92b1d52aca0c4803e220 __

--- home/_nvim/.config/nvim/lua/generic/key-bindings.lua
+++ home/_nvim/.config/nvim/lua/generic/key-bindings.lua
@@ -5,9 +5,8 @@ vim.keymap.set("n", "<leader><leader>g", ":tab Git<CR>", { noremap = true, silen
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>d", function()
-    vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
 end, { noremap = true, silent = true, desc = "Show diagnostics" })
-
 
 -- Move line
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -29,53 +28,75 @@ vim.keymap.set("n", "<C-i>", "<C-i>zz", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Find and replace current word
-vim.keymap.set("n", "<leader>rt", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "[R]ename [T]ext", noremap = true })
-vim.keymap.set("n", "<leader>rl", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "[R]ename [L]ine", noremap = true })
+vim.keymap.set(
+	"n",
+	"<leader>rt",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "[R]ename [T]ext", noremap = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>rl",
+	[[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "[R]ename [L]ine", noremap = true }
+)
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 vim.keymap.set("n", "<leader>%", function()
-    vim.cmd(":w")
-    vim.cmd("source %")
+	vim.cmd(":w")
+	vim.cmd("source %")
 end, { desc = "[S]ource [%]" })
 
 local function run_command()
-    local cwd = vim.fn.getcwd()
-    local run_sh_path = cwd .. "/run.sh"
-    if vim.fn.filereadable(run_sh_path) == 1 then
-        vim.cmd("!echo && bash " .. run_sh_path)
-    else
-        print("No .run.sh found in the current directory.")
-    end
+	local cwd = vim.fn.getcwd()
+	local run_sh_path = cwd .. "/run.sh"
+	if vim.fn.filereadable(run_sh_path) == 1 then
+		vim.cmd("!echo && bash " .. run_sh_path)
+	else
+		print("No .run.sh found in the current directory.")
+	end
 end
 
-vim.keymap.set('n', '<leader><leader>r', run_command, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader><leader>r", run_command, { noremap = true, silent = true })
 
 vim.api.nvim_create_user_command("WA", "wa", {})
 vim.api.nvim_create_user_command("Wa", "wa", {})
 vim.api.nvim_create_user_command("Q", "q", {})
 
-vim.keymap.set('n', '<leader>td', ':edit ~/.todo/TODO.md<CR>', { desc = "Edit TODO.md", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>td", ":edit ~/.todo/TODO.md<CR>", { desc = "Edit TODO.md", noremap = true, silent = true })
 -- Make executable
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "sh", "zsh" },
-    callback = function()
-        vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>",
-            { desc = "Make executable", noremap = true, silent = true })
-    end,
+	pattern = { "sh", "zsh" },
+	callback = function()
+		vim.keymap.set(
+			"n",
+			"<leader>x",
+			"<cmd>!chmod +x %<CR>",
+			{ desc = "Make executable", noremap = true, silent = true }
+		)
+	end,
 })
+
+vim.api.nvim_create_user_command("JdtlsClean", function()
+	local workspace_root = vim.fn.stdpath("data") .. "/site/java/workspace-root/"
+	if vim.fn.isdirectory(workspace_root) == 1 then
+		os.execute("rm -rf " .. workspace_root .. "*")
+		print("All JDT LS workspaces removed from " .. workspace_root)
+	else
+		print("Workspace root not found.")
+	end
+end, {})
