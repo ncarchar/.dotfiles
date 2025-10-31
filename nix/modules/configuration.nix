7be7315251a7dca71e8d1138d653a448aa7a8b53
Carson Miller <carsoncmiller@proton.me>
2025-10-31 16:14:15 -0400

    b0bd50577aa4f3f78fbe96b209657e5a __

--- nix/modules/configuration.nix
+++ nix/modules/configuration.nix
@@ -122,17 +122,6 @@
 
   hardware.graphics = {
     enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-    ];
-  };
-  hardware.amdgpu.opencl.enable = true;
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
     extraPackages = with pkgs; [
       rocmPackages.clr
       rocmPackages.clr.icd
