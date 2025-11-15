7737f35d7a7cbffa4bf7a8da1f69fa8cd1a9c62b
Carson Miller <carsoncmiller@proton.me>
2025-11-14 20:30:23 -0500

    43125881807ebdd3697e3ab34c674033 __

--- nix/modules/hardware-configuration.nix
+++ nix/modules/hardware-configuration.nix
@@ -5,9 +5,10 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "thunderbolt" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "thunderbolt" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  # boot.initrd.availableKernelModules = [ "nvme" "thunderbolt" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  # boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
