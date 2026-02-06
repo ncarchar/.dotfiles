02caa3fd53c0bac7a8fd7aae6006eb78a2344209
Carson Miller <carsoncmiller@proton.me>
2026-02-06 07:52:31 -0500

    b26ad4d4210b799e5143d458bae81ba9 __

--- home/_nvim/.config/nvim/lua/config/vim_settings.lua
+++ home/_nvim/.config/nvim/lua/config/vim_settings.lua
@@ -40,17 +40,32 @@ vim.o.foldminlines = 12
 vim.o.clipboard = "unnamedplus"
 -- Required to properly use system clipboard using WSL
 if vim.fn.has("win32") == 0 and os.getenv("WSL_DISTRO_NAME") then
+    local clip = "/mnt/c/Windows/System32/clip.exe"
+    local powershell = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+
     vim.g.clipboard = {
         name = "windows-clipboard",
         copy = {
-            ["*"] = "/mnt/c/WINDOWS/system32/clip.exe",
-            ["+"] = "/mnt/c/WINDOWS/system32/clip.exe",
+            ["+"] = { "sh", "-c", "iconv -f UTF-8 -t UTF-16LE | " .. clip },
+            ["*"] = { "sh", "-c", "iconv -f UTF-8 -t UTF-16LE | " .. clip },
         },
         paste = {
-            ["*"] = '/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ["+"] = '/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ["+"] = {
+                powershell,
+                "-NoProfile",
+                "-Command",
+                "[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new(); "
+                    .. 'Get-Clipboard -Raw | ForEach-Object { $_ -replace "`r", "" }',
+            },
+            ["*"] = {
+                powershell,
+                "-NoProfile",
+                "-Command",
+                "[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new(); "
+                    .. 'Get-Clipboard -Raw | ForEach-Object { $_ -replace "`r", "" }',
+            },
         },
-        cache_enabled = 1,
+        cache_enabled = 0,
     }
 end
 
